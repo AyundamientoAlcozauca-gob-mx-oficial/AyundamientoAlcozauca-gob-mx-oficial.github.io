05027b372e687bc893f95891b8e66e23519f8446
Albert <101471912+Albertteal@users.noreply.github.com>
2025-11-29 21:57:23 -0600

    Update permisos-db.js

--- js/permisos-db.js
+++ js/permisos-db.js
@@ -145,6 +145,8 @@ const PERMISOS_DB = {
         imagen: '/images/img-permiso-certificado.png'
     }
 
+
+    
     // Para agregar mas permisos, simplemente agrega una coma despues del ultimo objeto
     // y crea un nuevo objeto siguiendo el mismo formato:
     // 'ALCO-XXXXX': {
